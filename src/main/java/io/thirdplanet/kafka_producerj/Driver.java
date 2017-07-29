ef2d893bd27b89ae89ce904478dbd9aca0186afe
mikeyb <MichaelBrown172@Gmail.com>
2017-07-29 13:57:36 -0400

    ...

--- src/main/java/io/thirdplanet/kafka_producerj/Driver.java
+++ src/main/java/io/thirdplanet/kafka_producerj/Driver.java
@@ -1,10 +1,22 @@
 package io.thirdplanet.kafka_producerj;
 
+import java.util.Properties;
+
 /**
  * Created by mikeyb on 7/29/17.
+ * https://kafka.apache.org/0100/javadoc/index.html?org/apache/kafka/clients/producer/KafkaProducer.html
  */
 public class Driver {
     public static void main(String args[]) {
         System.out.println("Runs");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     }
 }
