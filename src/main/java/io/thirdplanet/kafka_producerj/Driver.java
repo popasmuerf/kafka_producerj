a72befb394f4386b0b3283790aa9c77be317212b
mikeyb <MichaelBrown172@Gmail.com>
2017-07-29 13:31:12 -0400

    ...

---
+++ src/main/java/io/thirdplanet/kafka_producerj/Driver.java
@@ -0,0 +1,10 @@
+package io.thirdplanet.kafka_producerj;
+
+/**
+ * Created by mikeyb on 7/29/17.
+ */
+public class Driver {
+    public static void main(String args[]) {
+        System.out.println("Runs");
+    }
+}
