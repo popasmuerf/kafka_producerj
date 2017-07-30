439d4f60f9a6c43548ffadcd2e68452bc40cd9b6
mikeyb <MichaelBrown172@Gmail.com>
2017-07-30 13:57:33 -0400

    ....

--- src/main/java/io/thirdplanet/kafka_producerj/Driver.java
+++ src/main/java/io/thirdplanet/kafka_producerj/Driver.java
@@ -1,15 +1,12 @@
 package io.thirdplanet.kafka_producerj;
 
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
-
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.Metric;
-import org.apache.kafka.common.MetricName;
-import org.apache.kafka.common.PartitionInfo;
 
-import java.util.*;
-import java.util.concurrent.Future;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,6 +15,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Driver {
     private static String topicName = "jproducer";
+    private static String fpath = "/home/mikeyb/data/text_files/logs/pfsense";
     public static void main(String args[]) {
         System.out.println("Runs");
         Properties props = new Properties();
@@ -34,20 +32,17 @@ public class Driver {
         String topic = "pfsense";
 
        for(int i = 0; i < 100 ; i++){
-          // producer.send(new ProducerRecord<String, String>(topicName,
-            //       Integer.toString(i), Integer.toString(i)));
-           //System.out.println("Message sent succesfully ");
-           //producer.close();
            Random rnd = new Random();
-
            long runtime = new Date().getTime();
-
            String ip = "192.168.1";
            int rndInt = rnd.nextInt(255);
            String rndIntStr = new Integer(rndInt).toString();
-           String msg = "http://www.example.com:" + ip  + rndIntStr;
+           String msg = "Jul 13 17:46:51 pfSense filterlog: 5,16777216,,1000000103,xn0,match,block,in,4,0x0,,47,0,0,DF,17,udp,129,37.134.157.194,10.0.0.56,63105,25148,109";
+           //String msg = "http://www.example.com:" + ip  + rndIntStr;
            ProducerRecord<String,String> producerRecord = new ProducerRecord<String, String>(topic,ip,msg);
            producer.send(producerRecord);
+           try{ TimeUnit.SECONDS.sleep(1);}catch(InterruptedException e ){}
+
        }
        producer.close();
     }
